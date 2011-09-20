cb003cf3eebcaf306749966c82998862710e3525
Søren Louv-Jansen <sl@konscript.com>
2011-09-20 20:27:18 +0200

    Added themes

---
+++ wp-content/themes/wooden-mannequin/footer.php
@@ -0,0 +1,29 @@
+<div id="footer">
+
+<div id="footer2">
+	<div id="footer2Right"><p style="margin-bottom:10px;">Certified Adviser:</p><img src="<?php bloginfo('stylesheet_directory'); ?>/img/qx_firstnorth.jpg" align="right"></div>
+	<div id="footer2Left">
+		<p class="footer2heading">Mere om k&oslash;b og salg af virksomheder inden for:</p>
+			<ul>
+				<li><a href="<?php bloginfo('url'); ?>/?page_id=37">Service</a>&nbsp;&nbsp;
+				<li><a href="<?php bloginfo('url'); ?>?page_id=43">Industri</a>&nbsp;&nbsp;
+				<li><a href="<?php bloginfo('url'); ?>?page_id=45">Handel</a>&nbsp;&nbsp;
+				<li><a href="<?php bloginfo('url'); ?>?page_id=47">Bygge- og anl&aelig;g</a>&nbsp;&nbsp;
+				<li><a href="<?php bloginfo('url'); ?>?page_id=49">IT, tele og konsulent</a>&nbsp;&nbsp;
+			</ul>
+			
+	</div>
+</div>
+
+<div id="footerinside">
+<p><span class="credits">&copy; <?php echo date('Y'); ?> <a href="<?php bloginfo('home'); ?>"><?php bloginfo('name'); ?></a> - </span> A/S | Kongevejen 153 | DK 2830 Virum | Tlf.: +45 43 58 33 00 | <a href="mailto:info@schroderpartners.dk">info@schroderpartners.dk</p>
+</div>
+</div>
+
+<?php do_action('wp_footer'); ?>
+
+</div>
+
+</body>
+
+</html>
---
+++ wp-content/themes/wooden-mannequin/navigation.php
@@ -0,0 +1,10 @@
+<p style="text-align:center; margin-top:10px; margin-left:25px; margin-right:20px;">
+<a class="navigationLinks" href="/?page_id=10">Referencer</a>
+<a class="navigationLinks" href="/?page_id=81">Profil</a>
+<a class="navigationLinks" href="/?page_id=112">Brancheviden</a>
+<a class="navigationLinks" href="/?page_id=22">Salg af virksomhed</a>
+<a class="navigationLinks" href="/?page_id=24">K&oslashb af virksomhed</a>
+<a class="navigationLinks" href="/?page_id=127">Kapitaltilf&oslashrsel</a>
+<a class="navigationLinks" href="/?page_id=129">Nyheder</a>
+<a class="navigationLinks" href="/?page_id=131">Kontakt</a>
+</p>
