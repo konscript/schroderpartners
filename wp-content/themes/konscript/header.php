50c36f02ff10228f0f6febe795db4a97351f18e6
Søren Louv-Jansen <sl@konscript.com>
2011-12-20 15:55:01 +0100

    moved GA to header

--- wp-content/themes/konscript/header.php
+++ wp-content/themes/konscript/header.php
@@ -20,6 +20,19 @@
 	<link rel="stylesheet" href="<?php bloginfo( 'template_url' ); ?>/stylesheet/compiled/main.css" type="text/css" media="all" />
 	<script type="text/javascript" src="<?php bloginfo( 'template_url' ); ?>/javascript/main.js"></script>
 	<?php wp_head(); // wp_head ?>
+	<script type="text/javascript">
+
+		var _gaq = _gaq || [];
+		_gaq.push(['_setAccount', 'UA-27849778-1']);
+		_gaq.push(['_trackPageview']);
+
+		(function() {
+		  var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+		  ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+		  var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+		})();
+
+	</script>	
 	
 </head>
 <body class="bp <?php hybrid_body_class(); ?>">
